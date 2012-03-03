6ae0afba25a9c39fb83b8e9535be4d9d28ae0ed2
Henrik N <henrik@nyh.se>
2012-03-03 17:42:40 +0100

    Cleanup.

--- README.markdown
+++ README.markdown
@@ -1,6 +1,18 @@
-## Hipchat emoticons Sinatra app on Heroku
+## Hipchat emoticons
 
-## Updating emoticons.json:
+A Sinatra app running on Heroku.
+
+See it here: <http://hipchat-emoticons.heroku.com>
+
+
+## Running locally
+
+    bundle install
+    ruby app.rb
+    open http://localhost:4567
+
+
+## Updating emoticons.json
 
 Run `rake` and follows its instructions.
 
@@ -8,8 +20,33 @@ The bookmarklet gets the info that the script needs. It includes a token that ex
 
 The task will update `emoticons.json`. Commit and push.
 
+
 ## Updating standard\_emoticons.json:
 
 I just looked at the HTML and built the file manually.
-If these ever change, do the same.
+If these ever change, do it again.
 
+
+## Credits and license
+
+By [Henrik Nyh](http://henrik.nyh.se/) under the MIT license:
+
+>  Copyright (c) 2011 Henrik Nyh
+>
+>  Permission is hereby granted, free of charge, to any person obtaining a copy
+>  of this software and associated documentation files (the "Software"), to deal
+>  in the Software without restriction, including without limitation the rights
+>  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+>  copies of the Software, and to permit persons to whom the Software is
+>  furnished to do so, subject to the following conditions:
+>
+>  The above copyright notice and this permission notice shall be included in
+>  all copies or substantial portions of the Software.
+>
+>  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+>  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+>  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+>  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+>  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+>  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+>  THE SOFTWARE.
--- app.rb
+++ app.rb
@@ -8,18 +8,58 @@ Bundler.require :default, (ENV['RACK_ENV'] || "development").to_sym
 set :haml, :format => :html5, :attr_wrapper => %{"}
 set :views, lambda { root }
 
-module Sorting
-  RECENT = "recent"
-end
 
 get '/' do
   # Cache in Varnish: http://devcenter.heroku.com/articles/http-caching
   headers 'Cache-Control' => 'public, max-age=3600'
 
-  @standard_emoticons = standard_emoticons
-  @secret_emoticons = secret_emoticons(params[:order])
-  @updated_on = [ File.mtime("./emoticons.json"), File.mtime("./standard_emoticons.json") ].max
+  @standard_emoticons = EmoticonFile.standard.emoticons(params[:order])
+  @secret_emoticons   = EmoticonFile.secret.emoticons(params[:order])
+  @updated_at = [ EmoticonFile.standard.updated_at, EmoticonFile.secret.updated_at ].max
   haml :index
+end
+
+
+class EmoticonFile
+  BY_RECENT    = "recent"
+
+  def self.standard
+    self.new("./standard_emoticons.json", :standard => true)
+  end
+
+  def self.secret
+    self.new("./emoticons.json", :standard => false)
+  end
+
+  def initialize(file, opts={})
+    @file = file
+    @standard = opts[:standard]
+  end
+
+  def emoticons(order=nil)
+    es = json.map { |e| Emoticon.new(e, :standard => @standard) }
+    if @standard
+      es  # Same order as in file.
+    else
+      if order == BY_RECENT
+        es.reverse
+      else  # Alphabetical.
+        es.sort_by { |x| x.shortcut }
+      end
+    end
+  end
+
+  def updated_at
+    File.mtime(@file)
+  end
+
+  private
+
+  def json
+    raw = File.read(@file)
+    json = JSON.parse(raw)
+    json["emoticon"]
+  end
 end
 
 class Emoticon
@@ -31,12 +71,12 @@ class Emoticon
   def initialize(data, opts={})
     @standard = opts[:standard]
 
-    @id = data["id"]
-    @path = data["path"]
+    @id       = data["id"]
+    @path     = data["path"]
+    @width    = data["width"].to_i
+    @height   = data["height"].to_i
     @shortcut = data["shortcut"]
     @shortcut.sub!(/^\((.+)\)$/, '\1') unless @standard
-    @width = data["width"].to_i
-    @height = data["height"].to_i
   end
 
   def url
@@ -47,25 +87,4 @@ class Emoticon
     @standard ? shortcut : "(#{shortcut})"
   end
 
-end
-
-def standard_emoticons
-  emoticons_from_file("./standard_emoticons.json").map { |e| Emoticon.new(e, :standard => true) }
-end
-
-def secret_emoticons(order)
-  emoticons = emoticons_from_file("./emoticons.json").map { |e| Emoticon.new(e, :standard => false) }
-  case order
-  when Sorting::RECENT
-    emoticons = emoticons.reverse
-  else  # Alphabetical.
-    emoticons = emoticons.sort_by { |x| x.shortcut }
-  end
-  emoticons
-end
-
-def emoticons_from_file(file)
-  raw = File.read(file)
-  json = JSON.parse(raw)
-  json["emoticon"]
 end
