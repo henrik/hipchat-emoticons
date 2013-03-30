355b0327c6afc00fd5b7953a854d9bb8ed062d2e
Henrik N <henrik@nyh.se>
2013-03-30 16:37:37 +0100

    Copy-to-clipboard with Flash.
    Thanks to @phatograph for making the initial version of this
    in pull request #4.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,49 @@
 $(function() {
 
+  // Notifications.
+
+  var $notification = $(".notification");
+  var notificationTimeout;
+  function notify(text) {
+    clearTimeout(notificationTimeout);
+    $notification.text(text).fadeIn();
+    notificationTimeout = setTimeout(function() { $notification.fadeOut() }, 1500);
+  }
+
+
+  // Clipboard.
+
+  var clip = new ZeroClipboard(null, {
+    moviePath: "/ZeroClipboard.swf",
+    hoverClass: "hover"
+  });
+
+  clip.on("noflash", function() {
+    $(".emoticon").hover(function() {
+      $(this).toggleClass("hover");
+    }).click(function() {
+      prompt("Copy this:", $(this).data("shortcut"));
+      return false;
+    });
+  });
+
+  clip.on("load", function() {
+    $(".emoticon").hover(function() {
+      var text = $(this).data("shortcut");
+      clip.setText(text);
+      clip.glue(this);
+    });
+  });
+
+  clip.on("complete", function(client, args) {
+    notify("Copied " + args.text);
+    // Let the browser steal back focus from Flash. Can't say I'm not proud ;p
+    $("<input>").css({opacity: 0}).appendTo(this).focus().remove();
+  });
+
+
+  // Filter.
+
   var $filter = $(".filter");
   var $container = $(".emoticons");
   var $emoticons = $(".emoticon");
@@ -7,11 +51,9 @@ $(function() {
   var FILTERED_OUT_CLASS = "filtered-out";
   var FILTERING_CLASS = "filtering";
 
-
   // search = on pressing "(x)" to clear.
   $filter.on("keyup change search", filterFromField);
   $filter.parent("form").on("submit", preventSubmit);
-
 
   function filterFromField() {
     filterTo(this.value);
